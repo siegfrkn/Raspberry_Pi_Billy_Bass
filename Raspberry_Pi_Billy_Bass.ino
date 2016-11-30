9a2c731fddf38a457e81005a8bdf7f123aaf1918
siegfrkn <katrina.siegfried@gmail.com>
2016-11-29 20:49:54 -0500

    Added Head/Tail Functionality
    This includes the segment of code that adds head/tail motor
    functionality. It moves the head up when Alexa speaks, and puts it down
    when she is done. There is commented out code that does not work but has
    not yet been removed.

--- Raspberry_Pi_Billy_Bass.ino
+++ Raspberry_Pi_Billy_Bass.ino
@@ -51,7 +51,6 @@ void setup() // the setup routine runs once when you press reset:
   headtailMotor->run(FORWARD); // turn on mouth motor
   headtailMotor->run(RELEASE); // release mouth motor
   pinMode(SoundInPin, INPUT);  // sound from the alexa coming into the jack is INPUT
-
 }
 
 void loop() // the loop routine runs over and over again forever:
@@ -63,19 +62,41 @@ void loop() // the loop routine runs over and over again forever:
   sensorValue = map(sensorValue,0,512,0,180); // We Map it here down to the possible range of  movement.
   // note normally the 512 is 1023 because of analog reading should go so far, but I changed that to get better readings. -Donald Bell
   
-  int MoveDelayValue = map(sensorValue,0,255,0,sensorValue); // map the MoveDelayValue as the max value, not sure where this is used now*   
+  //int MoveDelayValue = map(sensorValue,0,255,0,sensorValue); // map the MoveDelayValue as the max value, not sure where this is used now*   
 
-//  // Controlling head/tail motor movement
-//   while (sensorValue > 11) // 11 is the threshold at which the head lifts
-//   {
-//     headtailMotor->setSpeed(200); // set the head/tail motor speed to 200 out of range 0 to 255
-//     delay(1); // static delay to smooth things out
-//     headtailMotor->run(FORWARD); // move the head up (could be tail if you wired in opposite configuration)
-//     delay(1); // static delay to smooth things out
-//     //headtailMotor->setSpeed(0); // trying to turn off the goddamend head
-//     headtailMotor->run(RELEASE); // stop moving the head/tail motor
-//     delay(1); // static delay to smooth things out
-//   }
+//  // Controlling head/tail motor movement TEST
+//  delay(10);
+//  headtailMotor->setSpeed(200);
+//  delay(2000);
+//  headtailMotor->run(FORWARD);
+//  delay(2000);
+//  headtailMotor->run(RELEASE);
+//  delay(200);
+
+//// Controlling head/tail motor movement
+//  do
+//  {
+//  delay(1);
+//  headtailMotor->setSpeed(200);
+//  delay(1);
+//  headtailMotor->run(FORWARD);
+//  delay(1);
+//  headtailMotor->run(RELEASE);
+//  } while (sensorValue > 11);
+//  headtailMotor->run(RELEASE);
+
+
+   if (sensorValue > 13) // 11 is the threshold at which the head lifts
+   {
+     delay(1);
+     headtailMotor->setSpeed(200); // set the head/tail motor speed to 200 out of range 0 to 255
+     delay(1); // static delay to smooth things out
+     headtailMotor->run(FORWARD); // move the head up (could be tail if you wired in opposite configuration)
+     delay(2000); // static delay to smooth things out
+     //headtailMotor->setSpeed(0); // trying to turn off the goddamend head
+     //headtailMotor->run(RELEASE); // stop moving the head/tail motor
+     delay(1); // static delay to smooth things out
+   }
    
 
   // Controlling mouth motor movement
