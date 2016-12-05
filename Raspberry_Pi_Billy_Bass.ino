8aef840f575a61e6e69711afcb3afbfd21fa2980
siegfrkn <katrina.siegfried@gmail.com>
2016-12-04 20:02:03 -0500

    Integrated Head/Tail Functionality
    This integrated the head/tail motor with the mouth motor. Present values
    for sensorValue and MoveDelayValue gains, delays, and if loop values are
    all based on 12V 1A power supply. May change with 9V 2A power supply.

--- Raspberry_Pi_Billy_Bass.ino
+++ Raspberry_Pi_Billy_Bass.ino
@@ -1,87 +1,183 @@
 /*
-  Make a DC Motor Move to Sound.
+   Make two DC Motors Move to Sound.
    This example code is in the public domain.
-   Created by Donald Bell, Maker Project Lab (2016).
+   Created by Katrina Siegfried 2016, based on
+   code by Donald Bell, Maker Project Lab (2016).
    Based on Sound to Servo by Cenk Özdemir (2012) 
    and DCMotorTest by Adafruit
 */
-// include the Adafruit motor shield library
+
+/* Things to note:
+   1. You must wait at least 5 seconds before initiating Alexa commands
+*/
+
+
+/* Things to add as of 11/29/2016:
+   1. intregrate head/tail motor - need 9V 2A power supply
+   2. add filter to smooth out noise
+   3. add billy bass speakers as audio output
+   4. set if sensorValues based on filtered numbers to adjust for volume?
+   5. How does Pi output volume vs echo dot?
+   6. Normalize volume input more effectively - MoveDelayValue?
+*/
+
+
+
+
+// Include the Adafruit motor shield v2 library
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
-// Or, create it with a different I2C address (say for stacking)
-// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
 
 // Select which 'port' M1, M2, M3 or M4. In this case, M1 for mouth and M2 for tail
-Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
-Adafruit_DCMotor *myOtherMotor = AFMS.getMotor(2);
+Adafruit_DCMotor *mouthMotor = AFMS.getMotor(1); // M1 is mouth motor, 64 kHz
+Adafruit_DCMotor *headtailMotor = AFMS.getMotor(2); // M2 is head/tail motor, 64 kHz
 
-// Some other Variables we need
+// Define global variables
 int SoundInPin = A0;
-int LedPin = 12; //in case you want an LED to activate while mouth moves
-
-// the setup routine runs once when you press reset:
-void setup() {
-  Serial.begin(9600);           // set up Serial library at 9600 bps
+unsigned long previousMillis = 0;
+const long interval = 3000;
 
 
-  AFMS.begin();  // create with the default frequency 1.6KHz
-  //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
+
+void setup() // the setup routine runs once when you press reset:
+{
   
-  // Set the speed to start, from 0 (off) to 255 (max speed)
-  myMotor->setSpeed(0); //mouth motor
-  myMotor->run(FORWARD);
-  // turn on motor
-  myMotor->run(RELEASE);
-     pinMode(SoundInPin, INPUT);
-     pinMode(LedPin, OUTPUT);
-  myOtherMotor->setSpeed(0); //tail motor
-  myOtherMotor->run(FORWARD);
-  // turn on motor
-  myOtherMotor->run(RELEASE);
-     pinMode(SoundInPin, INPUT);  
+  Serial.begin(9600); // set up Serial library baud rate at 9600 bps
+  
+  AFMS.begin();  // create motor library with the default frequency 1.6KHz
+  
+  // Set the speed to start, from 0 (off) to 255 (max speed) for both motors
+  mouthMotor->setSpeed(0); // mouth motor not moving
+  mouthMotor->run(FORWARD); // turn on mouth motor
+  mouthMotor->run(RELEASE); // release mouth motor
+  headtailMotor->setSpeed(0); // tail motor not moving
+  headtailMotor->run(FORWARD); // turn on mouth motor
+  headtailMotor->run(RELEASE); // release mouth motor
+  pinMode(SoundInPin, INPUT);  // sound from the alexa coming into the jack is INPUT
 }
 
-// the loop routine runs over and over again forever:
-void loop() {
-  uint8_t i;
-  
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(SoundInPin);
-// we Map another value of this for LED that can be a integer betwen 0..255 
-  int LEDValue = map(sensorValue,0,512,0,255);
-  // We Map it here down to the possible range of  movement.
-  sensorValue = map(sensorValue,0,512,0,180);
-  // note normally the 512 is 1023 because of analog reading should go so far, but I changed that to get better readings.
-  int MoveDelayValue = map(sensorValue,0,255,0,sensorValue);
 
-  // maping the same reading a little bit more down to calculate the time your motor gets
-if (sensorValue > 10) { // to cut off some static readings
-   delay(1);  // a static delay to smooth things out...
-// now move the motor 
-   myMotor->run(FORWARD);
-  for (i=140; i<255; i++) {
-    myMotor->setSpeed(i);  
-  
-  }
 
-//for (i=200; i!=0; i--) {
-//    myMotor->setSpeed(i);  
-//    delay(10);
+
+void loop() // the loop routine runs over and over again forever:
+{
+  uint8_t i; // define i as an integer variable
+  int sensorValue = analogRead(SoundInPin)*4; // read the input on analog pin A0 and add a gain of 30%
+  int MoveDelayValue = analogRead(SoundInPin)*4;
+  sensorValue = map(sensorValue,0,512,0,180); // We Map it here down to the possible range of  movement, note normally the 512 is 1023 because of analog reading should go so far, but I changed that to get better readings. -Donald Bell
+  MoveDelayValue = map(sensorValue,0,512,0,180); // map the MoveDelayValue as the max value, not sure where this is used now*   
+
+
+//  // Controlling head/tail motor movement **THIS IS WHAT WORKS**
+//   if (sensorValue > 13) // 11 is the threshold at which the head lifts
+//   {
+//     delay(1);
+//     headtailMotor->setSpeed(100); // set the head/tail motor speed to 200 out of range 0 to 255
+//     delay(1); // static delay to smooth things out
+//     headtailMotor->run(FORWARD); // move the head up (could be tail if you wired in opposite configuration)
+//     delay(5000); // static delay so the head waits long enough
+//     //headtailMotor->setSpeed(0); // trying to turn off the goddamend head
+//     headtailMotor->run(RELEASE); // stop moving the head/tail motor
+//     delay(1); // static delay to smooth things out
+//   }
+
+
+if (MoveDelayValue >= 9) // this is the threshold at which the head lifts WILL BE MOVEDELAYVALUE
+     { 
+     unsigned long currentMillis = millis();
+     Serial.println(MoveDelayValue);
+     unsigned long motorSpeed = 230;
+     if (currentMillis - previousMillis < interval)
+     {
+       headtailMotor->setSpeed(0);
+       headtailMotor->run(BRAKE);
+       Serial.println("Pause the hold");
+       Serial.println(MoveDelayValue);
+       Serial.println(currentMillis);
+       Serial.println(previousMillis);
+       delay(1);
+       headtailMotor->run(BRAKE);
+       delay(1);
+       //delay(2000);
+       previousMillis = currentMillis;
+     }
+     else
+     {
+       Serial.println("Move the head");
+       delay(1);
+       //Serial.println(currentMillis);
+       delay(1);
+       headtailMotor->run(RELEASE);
+       delay(1);
+       headtailMotor->setSpeed(motorSpeed); // set the head/tail motor speed to 200 out of range 0 to 255
+       delay(1); // static delay to smooth things out
+       //Serial.println(motorSpeed);
+       headtailMotor->run(FORWARD); // move the head up (could be tail if you wired in opposite configuration)
+       delay(350); // static delay so the head waits long enough
+       headtailMotor->setSpeed(0); // trying to turn off the goddamend head
+       headtailMotor->run(BRAKE); // stop moving the head/tail motor
+       Serial.println(MoveDelayValue);
+       Serial.println(currentMillis);
+       Serial.println(previousMillis);
+       //delay(1);
+       delay(500); // static delay to smooth things out
+       //headtailMotor->run(RELEASE);
+       previousMillis = currentMillis;
+     }
+
+    // Controlling mouth motor movement
+    if (sensorValue > 11) // to cut off some static readings, otherwise the jaw chatters from noise
+    { 
+     delay(1);  // a static delay to smooth things out
+     mouthMotor->run(FORWARD); // now move the motor
+     delay(1); // a static delay to smoothe things out
+     for (i=110; i<255; i++) // control speed ramping, start at 140, count by 1, until 254 to speed up motor
+    {
+      mouthMotor->setSpeed(i);  // set the motor speed to whatever the value of the integer i is
+    }    
+    //Release control of both motors  
+    mouthMotor->run(RELEASE); // stop moving the mouth motor
+    //headtailMotor->run(RELEASE); // stop moving the head/tail motor
+    delay(1); // static delay to smooth things out
+    }
+     
+   }
+   else
+   {
+    //Serial.println(sensorValue);
+    delay(1);
+    headtailMotor->setSpeed(0);
+    delay(1);
+    headtailMotor->run(RELEASE);
+    delay(1);
+    //Serial.println("Motor released ");
+   }
+
+
+
+//  // Controlling mouth motor movement
+//  if (sensorValue > 11) // to cut off some static readings, otherwise the jaw chatters from noise
+//  { 
+//   delay(1);  // a static delay to smooth things out
+//   mouthMotor->run(FORWARD); // now move the motor
+//   delay(1); // a static delay to smoothe things out
+//   for (i=110; i<255; i++) // control speed ramping, start at 140, count by 1, until 254 to speed up motor
+//  {
+//    mouthMotor->setSpeed(i);  // set the motor speed to whatever the value of the integer i is
+//  }    
+//  //Release control of both motors  
+//  mouthMotor->run(RELEASE); // stop moving the mouth motor
+//  //headtailMotor->run(RELEASE); // stop moving the head/tail motor
+//  delay(1); // static delay to smooth things out
 //  }
-  
-  analogWrite(LedPin, sensorValue); 
-         // and do that move in this delay time
-  
-  myMotor->run(RELEASE);
-  myOtherMotor->run(RELEASE);
-  delay(1);
-} // Done.
-   // turn off the led again.
-      analogWrite(LedPin, 0); 
-      // and this repeats all the time.
-}
 
+//headtailMotor->run(RELEASE); // trying to stop the head from being crazy
+
+}
+// Done
+
+// Katrina Siegfried 2016
